bf60c955b0503b8325b330c427e050bfa19a5f4a
Brian Roth <brian.roth@infor.com>
2016-05-13 14:01:00 -0500

    Custom 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :load_seasons
+  before_action :load_defaults
 
   def faq
     render :faq
   end
 
   def render_404
+    load_defaults
     respond_to do |format|
       format.html { render :file => "#{Rails.root}/public/404", :status => :not_found }
       format.xml  { head :not_found }
@@ -15,7 +16,7 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  def load_seasons
+  def load_defaults
     @season = Season.default.first
     @division = @season.divisions.default.first
     @seasons = Season.all
